73e158fbe9dc73c76ec8878aae1f837d3d4bba06
Jean-Christian Denis <contact@jcdenis.fr>
2025-03-17 00:06:22 +0100

    release 1.0

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an empty plugin.
+It is used in Dotclear repository to set default tags.
---
+++ _define.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ * @brief       The plugin emptyPlugin definition
+ * @ingroup     emptyPlugin
+ *
+ * @defgroup    emptyPlugin Plugin emptyPlugin.
+ *
+ * This is an empty plugin.
+ *
+ * @author      Jean-Christian Denis (author)
+ * @copyright   AGPL-3.0
+ */
+declare(strict_types=1);
+
+$this->registerModule(
+    'Empty plugin',
+    'This is an empty plugin',
+    'Jean-Christian Denis',
+    '1.0',
+    [
+        //'requires'    => [['core', '2.33']],
+        'permissions' => null,
+        'type'        => 'plugin',
+    ]
+);
